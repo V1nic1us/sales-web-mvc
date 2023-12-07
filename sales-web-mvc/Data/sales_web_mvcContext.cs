ecac24a557edd469b7fe4a0e8e0789d1b0ef5842
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-06 22:28:39 -0400

    feat: add new tables to the context

--- sales-web-mvc/Data/sales_web_mvcContext.cs
+++ sales-web-mvc/Data/sales_web_mvcContext.cs
@@ -14,6 +14,8 @@ namespace sales_web_mvc.Data
         {
         }
 
-        public DbSet<sales_web_mvc.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
