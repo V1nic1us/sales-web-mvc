65929e6b3f73e6ae1342f4c2f6aa424946fff0c3
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-10 22:56:40 -0400

    feat: Add SellersController with Index action

---
+++ sales-web-mvc/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sales_web_mvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
