72e163ff259c03a5915452d29f31e0dd97126fa2
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-13 01:36:26 -0400

    feat: Added SellerService class for interacting with the database

---
+++ sales-web-mvc/Service/SellerService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sales_web_mvc.Data;
+using sales_web_mvc.Models;
+
+namespace sales_web_mvc.Service
+{
+    public class SellerService
+    {
+        private readonly sales_web_mvcContext _context;
+        
+        public SellerService(sales_web_mvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
