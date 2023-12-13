0507ab803024e7f6902890ecf68d8cbbc364d08c
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-13 01:14:09 -0400

    feat: BirthDate column name correction

---
+++ sales-web-mvc/Migrations/20231213051236_BirthDateColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sales_web_mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class BirthDateColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BithDate",
+                table: "Seller",
+                newName: "BirthDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BirthDate",
+                table: "Seller",
+                newName: "BithDate");
+        }
+    }
+}
--- sales-web-mvc/Models/Seller.cs
+++ sales-web-mvc/Models/Seller.cs
@@ -5,17 +5,17 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public DateTime BithDate { get; set; }
+        public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
         public Seller() { }
-        public Seller(int id, string name, string email, DateTime bithDate, double baseSalary, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
-            BithDate = bithDate;
+            BirthDate = birthDate;
             BaseSalary = baseSalary;
             Department = department;
         }
