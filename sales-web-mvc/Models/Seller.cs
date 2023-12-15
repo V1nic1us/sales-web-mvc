3922c83071c3d6ceb7d4f2eb0a54c3b56de7f897
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-14 22:43:32 -0400

    feat: Update the Seller class, adding Department property initialization

--- sales-web-mvc/Models/Seller.cs
+++ sales-web-mvc/Models/Seller.cs
@@ -7,7 +7,7 @@
         public string Email { get; set; }
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
-        public Department Department { get; set; }
+        public Department Department { get; set; } = new Department();
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
         public Seller() { }
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
