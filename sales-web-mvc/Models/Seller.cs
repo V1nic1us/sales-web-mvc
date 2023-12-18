fca15901a9e8fd9548dbbd61a53363933956372e
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-17 23:17:41 -0400

    feat: Add DepartmentId to the Seller class

--- sales-web-mvc/Models/Seller.cs
+++ sales-web-mvc/Models/Seller.cs
@@ -7,7 +7,8 @@
         public string Email { get; set; }
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
-        public Department Department { get; set; } = new Department();
+        public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
         public Seller() { }
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
