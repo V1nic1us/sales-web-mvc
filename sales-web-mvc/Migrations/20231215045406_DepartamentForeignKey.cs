77d2a365b60e5edb24cdd79c4d43ff83311a0d66
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-17 23:24:37 -0400

    feat: add a foreign key to the seller table

---
+++ sales-web-mvc/Migrations/20231215045406_DepartamentForeignKey.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sales_web_mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class DepartamentForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
