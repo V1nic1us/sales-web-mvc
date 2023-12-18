c5c6d034abe1e81432642d57a9327a2621deea2e
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-17 23:21:12 -0400

    feat: Add Department Service for handling departments

---
+++ sales-web-mvc/Service/DepartmentService.cs
@@ -0,0 +1,20 @@
+﻿using sales_web_mvc.Data;
+using sales_web_mvc.Models;
+
+namespace sales_web_mvc.Service
+{
+    public class DepartmentService
+    {
+        private readonly sales_web_mvcContext _context;
+
+        public DepartmentService(sales_web_mvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
