87dff4ca28829ea572ff8421572961a9adcc6f67
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-17 23:18:48 -0400

    feat: Add Seller From ViewModel to represent seller form data

---
+++ sales-web-mvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace sales_web_mvc.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public List<Department> Departments { get; set; }
+    }
+}
