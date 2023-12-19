43547889fe728c7211aef1de85a1ef21a2e99698
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-18 23:37:09 -0400

    feat: Added NotFoundException and DbConcurrencyException exceptions

---
+++ sales-web-mvc/Service/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,9 @@
+﻿namespace sales_web_mvc.Service.Exceptions
+{
+	public class DbConcurrencyException: ApplicationException
+	{
+		public DbConcurrencyException(string message) : base(message)
+		{
+		}
+	}
+}
---
+++ sales-web-mvc/Service/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace sales_web_mvc.Service.Exceptions
+{
+	public class NotFoundException: ApplicationException
+	{
+		public NotFoundException(string message) : base(message)
+		{
+		}
+	}
+}
