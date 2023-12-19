5054373b199d0c779d4623e296eacdd1bad285bb
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-19 00:00:23 -0400

    feat: Updates to ErrorViewModel, adding 'Message' property

--- sales-web-mvc/Models/ViewModels/ErrorViewModel.cs
+++ sales-web-mvc/Models/ViewModels/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace sales_web_mvc.Models.ViewModels
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+        public string Message { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
