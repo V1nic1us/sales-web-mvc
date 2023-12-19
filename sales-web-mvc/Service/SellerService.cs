37b5b1738176ff243e20510413ccba94152af67f
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-18 23:41:08 -0400

    feat: Added Update action to SellerService

--- sales-web-mvc/Service/SellerService.cs
+++ sales-web-mvc/Service/SellerService.cs
@@ -1,39 +1,57 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using sales_web_mvc.Data;
 using sales_web_mvc.Models;
+using sales_web_mvc.Service.Exceptions;
 
 namespace sales_web_mvc.Service
 {
-    public class SellerService
-    {
-        private readonly sales_web_mvcContext _context;
-        
-        public SellerService(sales_web_mvcContext context)
-        {
-            _context = context;
-        }
+	public class SellerService
+	{
+		private readonly sales_web_mvcContext _context;
 
-        public List<Seller> FindAll()
-        {
-            return _context.Seller.Include(s => s.Department).ToList();
-        }
+		public SellerService(sales_web_mvcContext context)
+		{
+			_context = context;
+		}
 
-        public void Insert(Seller obj)
-        {
-            _context.Add(obj);
-            _context.SaveChanges();
-        }
+		public List<Seller> FindAll()
+		{
+			return _context.Seller.Include(s => s.Department).ToList();
+		}
 
-        public Seller FindById(int id)
-        {
+		public void Insert(Seller obj)
+		{
+			_context.Add(obj);
+			_context.SaveChanges();
+		}
+
+		public Seller FindById(int id)
+		{
 			return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
 		}
 
-        public void Remove(int id)
-        {
+		public void Remove(int id)
+		{
 			var obj = _context.Seller.Find(id);
 			_context.Seller.Remove(obj);
 			_context.SaveChanges();
 		}
-    }
+
+		public void Update(Seller obj)
+		{
+			if (!_context.Seller.Any(x => x.Id == obj.Id))
+			{
+				throw new NotFoundException("Id not found");
+			}
+			try
+			{
+				_context.Update(obj);
+				_context.SaveChanges();
+			}
+			catch (DbUpdateConcurrencyException e)
+			{
+				throw new DbConcurrencyException(e.Message);
+			}
+		}
+	}
 }
