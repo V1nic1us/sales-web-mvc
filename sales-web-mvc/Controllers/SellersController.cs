86a66b07e0dce6f143a50bc6e0a54ba86d8dbc46
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-18 23:02:54 -0400

    feat: Added Details action to SellersController

--- sales-web-mvc/Controllers/SellersController.cs
+++ sales-web-mvc/Controllers/SellersController.cs
@@ -52,5 +52,13 @@ namespace sales_web_mvc.Controllers
 			_sellerService.Remove(id);
 			return RedirectToAction(nameof(Index));
 		}
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null) return NotFound();
+			var obj = _sellerService.FindById(id.Value);
+			if (obj == null) return NotFound();
+			return View(obj);
+        }
     }
 }
