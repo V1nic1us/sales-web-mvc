1d1d2415e8583cbcb16cc79aa2d52c95c723e05b
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-18 23:39:04 -0400

    feat: Added Edit actions to SellersController

--- sales-web-mvc/Controllers/SellersController.cs
+++ sales-web-mvc/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using sales_web_mvc.Models;
 using sales_web_mvc.Models.ViewModels;
 using sales_web_mvc.Service;
+using sales_web_mvc.Service.Exceptions;
 
 namespace sales_web_mvc.Controllers
 {
@@ -60,5 +61,35 @@ namespace sales_web_mvc.Controllers
 			if (obj == null) return NotFound();
 			return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+			if (id == null) return NotFound();
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null) return NotFound();
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+			if (id != seller.Id) return BadRequest();
+			try
+            {
+				_sellerService.Update(seller);
+				return RedirectToAction(nameof(Index));
+			}
+			catch (NotFoundException e)
+            {
+				return NotFound();
+			}
+            catch (DbConcurrencyException e)
+            {
+                return BadRequest();
+            }
+		}
     }
 }
