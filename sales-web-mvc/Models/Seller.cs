fb0f969b4b48052e76384cefdf16a99c2a43f73e
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-26 00:57:05 -0400

    feat: Add validation annotations and custom error messages to attributes, ensuring consistency and validation across seller data.

--- sales-web-mvc/Models/Seller.cs
+++ sales-web-mvc/Models/Seller.cs
@@ -5,16 +5,23 @@ namespace sales_web_mvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required.")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1} characters.")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required.")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required.")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required.")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
