28a61a729becc8d79061d529c5016225946d0821
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-12-27 22:27:38 -0400

    feat: creation of the SalesRecordService and creation of the FindByDateAsync method

---
+++ sales-web-mvc/Service/SalesRecordService.cs
@@ -0,0 +1,38 @@
+﻿using sales_web_mvc.Data;
+using sales_web_mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace sales_web_mvc.Service
+{
+    public class SalesRecordService
+    {
+        private readonly sales_web_mvcContext _context;
+
+        public SalesRecordService(sales_web_mvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            // LINQ query
+            var result = from obj in _context.SalesRecord select obj;
+            // if minDate is not null, add a restriction to the query
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            // if maxDate is not null, add a restriction to the query
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            // return the query result
+            return await result
+                .Include(x => x.Seller) // join with Seller table
+                .Include(x => x.Seller.Department) // join with Department table
+                .OrderByDescending(x => x.Date) // order by Date descending
+                .ToListAsync(); // convert to list
+        }
+    }
+}
